f506b15e1900647089dcd59b81f678b9c12d99b8
Ricardo <ricardo.rg462@gmail.com>
2023-06-12 21:04:32 -0400

    exporting profits results to xlsx

--- calculo_anual.py
+++ calculo_anual.py
@@ -11,48 +11,71 @@ PROJECTIONS_FILE_NAME = 'proyecciones.xlsx'
 days_per_month = 365 / 12
 scenario = 'escenario_a'
 sheets_names = ['Aerogeneradores', 'Goldwind GW87 1500', 'Vestas V110 2000', 'Vestas V100 2000']
+aerogeneradores = sheets_names[1:4]
 columns_names = ['year', 'month', 'hora', 'wind_speed', 'rounded_wind_speed',
                   'potencia_aerogenerador_k', 'potencia_aerogenerador_m', 'energia_aerogenerador', 'energia_total']
 
-with cwd(DATA_PATH):
-    generacion = pd.read_excel(FILE_NAME, sheet_name=sheets_names[1], usecols='A:I', names=columns_names)
-    generacion.reset_index(inplace=True, drop=True)
-    projections = pd.read_excel(PROJECTIONS_FILE_NAME)
-    generacion = generacion.drop([i for i in range(8)])
+ganancia_aerogeneradores = []
+for aerogenerador in aerogeneradores:
+    print(aerogenerador)
+
+    with cwd(DATA_PATH):
+        generacion = pd.read_excel(FILE_NAME, sheet_name=aerogenerador, usecols='A:I', names=columns_names)
+        generacion.reset_index(inplace=True, drop=True)
+        projections = pd.read_excel(PROJECTIONS_FILE_NAME)
+        generacion = generacion.drop([i for i in range(8)])
+
+
+    df_scenario = projections[scenario].apply(pd.to_numeric)
+    energy = generacion['energia_total'].apply(pd.to_numeric)
+    energy.reset_index(inplace=True, drop=True)
+    money = df_scenario*energy
+    result = pd.DataFrame()
+    result['year'] = generacion['year'].reset_index(drop=True)
+    result['month'] = generacion['month'].reset_index(drop=True)
+    result['hora'] = generacion['hora'].reset_index(drop=True)
+    result['scenario'] = df_scenario
+    result['energy'] = energy
+    result['money'] = money
+    result.reset_index(inplace=True, drop=True)
+
+
+    money_per_months = []
+    money_per_years = []
+    for year in range(2024, 2044):
+        money_per_year = []
+        for month in ['enero', 'febrero' , 'marzo', 'abril', 'mayo', 'junio','julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']:
+            money_per_typical_day = result.loc[(result['year'] == year) & (result['month'] == month)]['money'].values
+            money_per_month = (money_per_typical_day * days_per_month).sum()
+            money_per_months.append(money_per_month)
+            money_per_year.append(money_per_month)
+
+        money_per_year = np.array(money_per_year)
+        money_per_year = money_per_year.sum()
+        money_per_years.append(money_per_year)
+
+    money_per_months = np.array(money_per_months)
+    money_per_years = np.array(money_per_years)
+
+
+    # Exportar resultados
+
+    years = pd.Series([year for year in range(2024, 2044)])
+    money_per_years = pd.Series(money_per_years)
+
+    profits = pd.DataFrame()
+    profits['year'] = years
+    profits['USD'] = money_per_years
+
+    ganancia_aerogeneradores.append(profits)
 
 
 
+#profits.to_excel(os.sep.join(['data', 'profits_' + 'goldwind' + '.xlsx']), index=False)
+Excelwriter = pd.ExcelWriter(os.sep.join([DATA_PATH, "ganancias.xlsx"]),engine="xlsxwriter")
 
-
-scenario = projections[scenario].apply(pd.to_numeric)
-energy = generacion['energia_total'].apply(pd.to_numeric)
-energy.reset_index(inplace=True, drop=True)
-money = scenario*energy
-result = pd.DataFrame()
-result['year'] = generacion['year'].reset_index(drop=True)
-result['month'] = generacion['month'].reset_index(drop=True)
-result['hora'] = generacion['hora'].reset_index(drop=True)
-result['scenario'] = scenario
-result['energy'] = energy
-result['money'] = money
-result.reset_index(inplace=True, drop=True)
-
-
-money_per_months = []
-money_per_years = []
-for year in range(2024, 2044):
-    money_per_year = []
-    for month in ['enero', 'febrero' , 'marzo', 'abril', 'mayo', 'junio','julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']:
-        money_per_typical_day = result.loc[(result['year'] == year) & (result['month'] == month)]['money'].values
-        money_per_month = (money_per_typical_day * days_per_month).sum()
-        money_per_months.append(money_per_month)
-        money_per_year.append(money_per_month)
-
-    money_per_year = np.array(money_per_year)
-    money_per_year = money_per_year.sum()
-    money_per_years.append(money_per_year)
-
-money_per_months = np.array(money_per_months)
-money_per_years = np.array(money_per_years)
-print(money_per_months.shape, money_per_months.sum())
-print(money_per_years.shape, money_per_years.sum())
+#We now we'll loop the list of dataframes
+for df, aerogenerador in zip (ganancia_aerogeneradores, aerogeneradores):
+    df.to_excel(Excelwriter, sheet_name=aerogenerador,index=False)
+#And finally we save the file
+Excelwriter.save()
